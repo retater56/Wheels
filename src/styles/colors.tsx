723a32806b8b8737b62002a27ba5b3ea5ce66592
Vladislav Shevtsov <70779585+retater56@users.noreply.github.com>
2022-03-09 21:04:01 +0300

    feat: create styles constants

---
+++ src/styles/colors.tsx
@@ -0,0 +1,16 @@
+const colors = {
+    white: '#FFF',
+    black: '#000',
+    gray: '#F9F9F9',
+    primary: '#212121',
+    primaryLight: '#484848',
+    primaryDark: '#000000',
+    secondary: '#8E24AA',
+    secondaryLight: '#c158dc',
+    secondaryDark: '#5c007a',
+    textPrimary: '#ffffff',
+    textSecondary: '#9E9E9E',
+    backgroundColor: '#212121',
+    borderColer: '#5C007A'
+  }
+  export default colors;
---
+++ src/styles/fontSizes.tsx
@@ -0,0 +1,8 @@
+const fontSizes = {
+    small: 14,
+    medium: 18,
+    large: 24,
+    big: 28
+}
+
+export default fontSizes;
